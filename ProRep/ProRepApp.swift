037052937e00d1bb370b2f84f4209c578f80007c
Qiang Loozen <qiangloozen@gmail.com>
2023-04-09 11:55:46 +0200

    Initial Commit

---
+++ ProRep/ProRepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProRepApp.swift
+//  ProRep
+//
+//  Created by Qiang Loozen on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProRepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
